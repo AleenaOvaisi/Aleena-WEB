88f4a4556aba451f797fef7a8f527699c5cbbc50
AleenaOvaisi <aleenaovaisi113@icloud.com>
2024-08-13 15:19:47 +0530

    third

--- index.js
+++ index.js
@@ -2,6 +2,27 @@
 //     document.write("Helluuuuuu");
 //  }
 
-function Greetings(){
-    document.getElementById("demo").innerHTML="Have a good day";
+// function Greetings(){
+//     document.getElementById("demo").innerHTML="Have a good day";
+// }
+
+function Morning(){
+    document.getElementById("demo").innerHTML="good Morning";
+    document.getElementById("mode").style.background="white";
+    document.getElementById("mode").style.color="black";
+}
+function Afternoon(){
+    document.getElementById("demo").innerHTML="afternooon";
+    document.getElementById("mode").style.background="pink";
+    document.getElementById("mode").style.color="white";
+}
+function Evening(){
+    document.getElementById("demo").innerHTML="evening";
+    document.getElementById("mode").style.background="blue";
+    document.getElementById("mode").style.color="white";
+}
+function Night(){
+    document.getElementById("demo").innerHTML="night";
+    document.getElementById("mode").style.background="black";
+    document.getElementById("mode").style.color="white";
 }
