11857abf928292847bdfd090db13f57d0350dbf3
AleenaOvaisi <aleenaovaisi113@icloud.com>
2024-08-13 14:12:28 +0530

    First

---
+++ aloo.js
@@ -0,0 +1,6 @@
+//document.write("welcome students");
+//console.log("hellllooooooooooo");
+
+let x=120;
+    let y=30;
+console.log(x,y);
---
+++ file1.js
@@ -0,0 +1,46 @@
+//let a=10;
+//document.write(a,"<br/>");
+//let b=5;
+//document.write(b,"<br/>");
+//let sum=a+b;
+//document.write(sum,"<br/>");
+//let difference=a-b;
+//document.write(difference,"<br/>");
+//let product=a*b;
+//document.write(product,"<br/>");
+//let quotient=a/b;
+//document.write(quotient,"<br/>");
+// let remainder=a%b;
+// document.write(remainder,"<br/>");
+// let isEqual=(a==b);
+// document.write(isEqual,"<br/>");
+// let isNotEqual=(a!=b);
+// document.write(isNotEqual,"<br/>");
+// let isGreater=(a>b);
+// document.write(isGreater,"<br/>");
+// let LogicalAnd=(a>b && b<10);
+// // document.write(LogicalAnd,"<br/>");
+//------------------------------------------------------//
+
+//  let fname="john";
+// document.write("hello",fname,"sir","<br>");
+
+// let age=30;
+// document.write("age",age,"<br>");
+
+// let isStudent=true;
+// document.write(`Student- ${isStudent?"yes":"no"}<br>`);
+
+// let emptyvalue="null";
+// document.write(`About us ${emptyvalue||"write something"} <br>`);
+
+// let uninitialized;
+// document.write(!uninitialized && "fill details completely");
+//--------------------------------------------------------------------//
+
+let  age=144;
+if(age>17){
+    document.write("eligible to vote");
+}else{
+    document.write("not eligible to vote");
+}
